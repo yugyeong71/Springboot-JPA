195cc57f2c2be2fc8ba47a70d09eedc8b3b05d97
고유경 <youkyoung71@naver.com>
2022-06-18 00:03:24 +0900

    Update : Tags test

--- src/test/java/com/example/demo/settings/SettingsControllerTest.java
+++ src/test/java/com/example/demo/settings/SettingsControllerTest.java
@@ -2,16 +2,23 @@ package com.example.demo.settings;
 
 import com.example.demo.WithAccount;
 import com.example.demo.account.AccountRepository;
+import com.example.demo.account.AccountService;
 import com.example.demo.domain.Account;
+import com.example.demo.domain.Tag;
+import com.example.demo.settings.form.TagForm;
+import com.example.demo.tag.TagRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -19,6 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("local")
@@ -27,10 +35,65 @@ public class SettingsControllerTest {
     @Autowired MockMvc mockMvc;
     @Autowired AccountRepository accountRepository;
     @Autowired PasswordEncoder passwordEncoder;
+    @Autowired ObjectMapper objectMapper;
+    @Autowired TagRepository tagRepository;
+    @Autowired AccountService accountService;
 
     @AfterEach
     void afterEach(){
         accountRepository.deleteAll();
+    }
+
+    @WithAccount("yugyeong")
+    @DisplayName("계정의 태그 수정 폼")
+    @Test
+    void updateTagsForm() throws Exception{
+        mockMvc.perform(get(SettingsController.SETTINGS_TAGS_URL))
+                .andExpect(view().name(SettingsController.SETTINGS_TAGS_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("whitelist"))
+                .andExpect(model().attributeExists("tags"));
+    }
+
+    @WithAccount("yugyeong")
+    @DisplayName("계정에 태그 추가")
+    @Test
+    void addTag() throws Exception{
+        TagForm tagForm = new TagForm();
+        tagForm.setTagTitle("newTag");
+
+        mockMvc.perform(post(SettingsController.SETTINGS_TAGS_URL + "/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(tagForm))
+                .with(csrf()))
+                .andExpect(status().isOk());
+
+        Tag newTag = tagRepository.findByTitle("newTag");
+        assertNotNull(newTag);
+        Account yugyeong = accountRepository.findByNickname("yugyeong");
+        assertTrue(yugyeong.getTags().contains(newTag));
+    }
+
+    @WithAccount("yugyeong")
+    @DisplayName("계정에 태그 삭제")
+    @Test
+    void removedTag() throws Exception{
+        Account yugyeong = accountRepository.findByNickname("yugyeong");
+        Tag newTag = tagRepository.save(Tag.builder().title("newTag").build());
+        accountService.addTag(yugyeong, newTag);
+
+        assertTrue(yugyeong.getTags().contains(newTag));
+
+        TagForm tagForm = new TagForm();
+        tagForm.setTagTitle("newTag");
+
+        mockMvc.perform(post(SettingsController.SETTINGS_TAGS_URL + "/remove")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tagForm))
+                        .with(csrf()))
+                .andExpect(status().isOk());
+
+        assertFalse(yugyeong.getTags().contains(newTag));
     }
 
     @WithAccount("yugyeong") // 없으면 동작하지 않음, 인증이 필요한 Test 라서
