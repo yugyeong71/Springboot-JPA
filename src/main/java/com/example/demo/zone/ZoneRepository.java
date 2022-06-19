08d4621ea4056bef2323e2825e91532d97507622
고유경 <youkyoung71@naver.com>
2022-06-19 22:20:32 +0900

    Update : Zone

---
+++ src/main/java/com/example/demo/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.zone;
+
+import com.example.demo.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
